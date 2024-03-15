3b04d4636998682b95c5e34f54eb6230d631ab2e
Gabriel Jankowiec <ghjankowiec@gmail.com>
2024-03-15 11:06:34 +0100

    ADD: MyPhotos page, modif NavBar routes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,12 +2,13 @@
 export const NavBar = () => {
     let items = ['home', 'myPhotos'];
     let icons = ['../src/assets/homePageIcon.png', '../src/assets/navBarFavoriteIcon.png'];
+    let links = ['/', '/myPhotos']
 
     const navItems = items.map((item, i) => {
         return (
             <li key={i} className="nav__list_item">
                 <img src={icons[i]} alt={item} className='icon'/>
-                <a href="/" id={item}>{item}</a>
+                <a href={links[i]} id={item}>{item}</a>
             </li>
         );
     })
--- src/pages/MyPhotos.jsx
+++ src/pages/MyPhotos.jsx
@@ -1,10 +1,17 @@
+import { NavBar } from "../components/NavBar";
+import { Footer } from "../components/footer_Components/Footer";
+import { Header } from "../components/header_Components/Header";
+import { Pictures_Section } from "../components/pictures_components/Pictures_Section";
 
 
 export const MyPhotos = () => {
 
     return (
         <>
-
+            <Header />
+            <NavBar />
+            <Pictures_Section />
+            <Footer />
         </>
     )
 };
